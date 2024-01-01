2735a3ddf8461775f336644555a9170b860db11a
Jhonatan Vargas <jona.vand98@gmail.com>
2024-01-01 15:21:37 -0600

    Mostrando posts y paginacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,8 +20,11 @@ class PostController extends Controller
     public function index(User $user)
     {
 
+        $posts = Post::where('user_id', $user->id)->paginate(5);
+
         return view('layouts.dashboard', [
             'user' => $user,
+            'posts' => $posts
         ]);
     }
 
